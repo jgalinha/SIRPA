e5899972faffcc4e66aa7564ebec404aacd2df28
José Galinha <jbgalinha@gmail.com>
2021-12-11 19:43:40 +0000

    :memo: (backend.sirpa): update docker-compose file to include fastApi service

---
+++ pyc_to_gitignore.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# search *.pyc files and add them to .gitignore file
+find -name "*.pyc" -exec echo "{}" >> .gitignore \;                                                                                                                                     sáb 11 dez 2021 19:39:11
